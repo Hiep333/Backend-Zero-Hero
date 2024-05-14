5af940f6285ab0aab9451ab07ec5c4a39bdfcbfc
Hiep <tothanhhiep.work@gmail.com>
2024-05-14 17:21:14 +0700

    khai bao route

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/abc', (req, res) => {
+  res.send('check abc!')
+})
+
+app.get('/hiep', (req, res) => {
+  res.send('<h1>hiep ne</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
