85ff25e2a2479b61cedc322e383614ba88c0ee34
Hiep <tothanhhiep.work@gmail.com>
2024-05-18 11:34:53 +0700

    use MySql with nodejs

--- src/server.js
+++ src/server.js
@@ -4,8 +4,11 @@ const express = require('express') // commonjs
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 
+const mysql = require('mysql2')
+
+
 const app = express() // app express
-const port = process.env.PORT || 9999 // port => hardcode
+const port = process.env.PORT || 8080 // port => hardcode
 const hostname = process.env.HOSTNAME || 'localhost'
 
 //config template engine
@@ -13,6 +16,21 @@ configViewEngine(app)
 
 //khai bao routes
 app.use('/', webRoutes)
+
+// test connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit',
+  port: 3307
+})
+
+//simple query
+connection.query('SELECT * FROM users', (err, result, fields) => {
+  console.log('>>>result',result);
+  console.log('>>>fields',fields);
+})
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening at http://${hostname}:${port}`)
